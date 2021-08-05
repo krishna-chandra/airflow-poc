0cd5acdd9a004fdb82b412abf32e467dfc05b996
Krshna <krishna>
2021-08-05 11:21:42 +0530

    putting sleep function

--- dags/s3_file_processor.py
+++ dags/s3_file_processor.py
@@ -1,4 +1,5 @@
 import boto3
+from time import sleep
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
@@ -8,16 +9,17 @@ from datetime import datetime, timedelta
 
 # simple download task
 def download_file():
-    s3_client = boto3.client('s3')
+    """ s3_client = boto3.client('s3')
     s3_client.download_file('airflow-xcom', 'airflow.cfg', 'downloaded_airflow_config.cfg')
     print('@'*100)
     print(open('downloaded_airflow_config.cfg').read())
-    print('@'*100)
+    print('@'*100) """
+    sleep(15)
 
 
 def start():
-    print("Downloading started ....")
-
+   """  print("Downloading started ....") """
+   sleep(2)
 
 # default arguments for each task
 default_args = {
@@ -46,6 +48,15 @@ with DAG(
         python_callable=download_file,
         )
 
+    download_from_s3_1 = PythonOperator(
+        task_id='download_from_s3_1',
+        python_callable=download_file,
+        )
+
+    download_from_s3_2 = PythonOperator(
+        task_id='download_from_s3_2',
+        python_callable=download_file,
+        )   
 
     start = PythonOperator(
         task_id="start",
@@ -55,3 +66,5 @@ with DAG(
 
 
 start >> download_from_s3
+start >> download_from_s3_1
+start >> download_from_s3_2
